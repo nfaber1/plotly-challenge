befc508cc263ab0563c51e6c963690394b4cd8d7
Nick Faber <nfaber@sandiego.edu>
2021-03-04 22:07:09 -0800

    finished the plots.

--- Homework/Instructions/StarterCode/static/js/app.js
+++ Homework/Instructions/StarterCode/static/js/app.js
@@ -56,9 +56,75 @@ function init() {
 
       });
     };
- 
 
-// };
+    function plots(name) {
+      // LOAD DATA
+      d3.json("samples.json").then((dataData) => {
+          var sampleDataData = dataData.samples;
+          var secID = sampleDataData.filter(object => object.id == name)[0];
+          var sampleValues = secID.sample_values;
+          // SEE IF THEY CAME UP
+          console.log(sampleDataData)
+          console.log(secID)
+          console.log(sampleValues)
+          
+          // // OTU ID VARIABLE & LABELS
+          var OTUID = secID.otu_ids;
+          var OTUlabel = secID.otu_labels;
+    
+          // CREATE TRACE1 FOR BAR CHART
+          var yticks = OTUID.slice(0, 10).map(object1 => `OTU ${OTUID}`).reverse();
+          var trace1 = {
+            x: sampleValues.slice(0,10).reverse(),
+            y: yticks,
+            type: 'bar',
+            // text: OTUlabel.slice(0,10).reverse(),
+            orientation: 'h'
+        };
+    
+        // CREATE TRACE1
+        var data1 = [trace1];
+
+      // });
+
+        var layout1 = {
+            title: 'TOP OTUs',
+            showlegend: true,
+            xaxis: {
+                tickangle: -45
+            },
+            yaxis: {
+                zeroline: false,
+                gridwidth: 2
+            },
+            bargap: 0.05
+        };
+    
+        Plotly.newPlot('bar', data1, layout1);
+
+        var trace2 = {
+          x: OTUID,
+          y: sampleValues,
+          mode: 'markers',
+          marker: {
+              size: sampleValues,
+              color: OTUID
+          }
+      };
+
+      // CREATE TRACE2 & LAYOUT
+      var data2 = [trace2]
+      var layout2 = {
+          title: 'Sample OTUs',
+          showlegend: false,
+          height: 600,
+          width: 1200,
+          margin: {t:30}
+      };
+
+      Plotly.newPlot('bubble', data2, layout2);
+      });
+};
 init();
 
 
